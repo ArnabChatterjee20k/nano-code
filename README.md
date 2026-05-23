ccb7cbe9f4807bc79c2b6efa80600c7b5015f6ee
ArnabChatterjee20k <arnabchatterjee.ac.2@gmail.com>
2026-05-23 23:34:01 +0530

    Initialize nano-code project with basic structure and functionality

---
+++ README.md
@@ -0,0 +1,3 @@
+A single file nano implementation of claude-code, codex, etc with support for tool calling, code repo indexing,etc
+
+/clear, /quit, /index
---
+++ src/main.rs
@@ -0,0 +1,67 @@
+use std::io::Write;
+use std::{fmt, io};
+#[allow(dead_code)]
+#[derive(Debug, Copy, Clone)]
+pub enum Ansi {
+    Reset,
+    Bold,
+    Dim,
+    Blue,
+    Cyan,
+    Green,
+    Yellow,
+    Red,
+}
+impl fmt::Display for Ansi {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let code = match self {
+            Ansi::Reset => "\x1b[0m",
+            Ansi::Bold => "\x1b[1m",
+            Ansi::Dim => "\x1b[2m",
+            Ansi::Blue => "\x1b[34m",
+            Ansi::Cyan => "\x1b[36m",
+            Ansi::Green => "\x1b[32m",
+            Ansi::Yellow => "\x1b[33m",
+            Ansi::Red => "\x1b[31m",
+        };
+        write!(f, "{}", code)
+    }
+}
+#[macro_export]
+macro_rules! log {
+    ($color:expr, $($arg:tt)*) => {{
+        println!("{}{}{}", $color, format_args!($($arg)*), Ansi::Reset);
+    }};
+}
+
+#[macro_export]
+macro_rules! log_separator {
+    () => {{
+        let width = 80;
+        let line = "─".repeat(width);
+        println!("{}{}{}", Ansi::Dim, line, Ansi::Reset)
+    }};
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    log!(Ansi::Bold, "nano-code");
+    loop {
+        log_separator!();
+        print!("{}{}❯{} ", Ansi::Bold, Ansi::Blue, Ansi::Reset);
+        io::stdout().flush()?;
+
+        let mut input = String::new();
+        let bytes = io::stdin().read_line(&mut input)?;
+        if bytes == 0 {
+            break;
+        }
+        match input.trim() {
+            "/q" => {
+                log!(Ansi::Cyan, "Thanks for using nano-code!");
+                break;
+            }
+            _ => {}
+        }
+    }
+    Ok(())
+}
